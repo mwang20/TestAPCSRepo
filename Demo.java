dfaa75a6391f8ca9a41ddf038024d64ace10d6ac
mwang20 <mwang20@stuy.edu>
2020-10-23 15:17:57 -0400

    I finished the create2DArray method. Also I revised the arrDeepToString method and added a bunch of printlns so the output would be cleaner

--- Demo.java
+++ Demo.java
@@ -1,11 +1,17 @@
+import java.util.Random;
 public class Demo{
   public static void main(String[] args){
     int pyramid = Integer.parseInt(args[0]);
     printLoop(pyramid);
+    System.out.println("");
     int[] n = {5, 4, 3, 2, 1};
     int[][] test = {{5, 4, 3, 2, 1},{1, 2, 3, 4, 5}};
     arrToString(n);
+    System.out.println("");
     arrDeepToString(test);
+    System.out.println("");
+    arrDeepToString(create2DArray(2, 2, 4));
+    System.out.println("");
   }
   public static void printLoop(int n){
     for(int i = 1; i <= n; i++ ){
@@ -21,7 +27,21 @@ public class Demo{
    }
    public static void arrDeepToString(int [][]arr){
       System.out.println();
-      for ( int i = 0; i < arr.length; i++ )
-          System.out.print(arr[i][i]);
+      for ( int i = 0; i < arr.length; i++ ){
+        for ( int j = 0; j < arr[i].length; j++ ){
+            System.out.print(arr[i][j] + " ");
+          }
+          System.out.println();
+      }
+    }
+   public static int[][] create2DArray(int rows, int cols, int maxValue){
+     Random rand = new Random();
+     int[][] new2DArray = new int[rows][cols];
+     for (int i = 0; i < rows; i++){
+       for (int j = 0; j < cols; j++){
+         new2DArray [i][j] = rand.nextInt(maxValue + 1);
+       }
+     }
+     return new2DArray;
    }
 }
