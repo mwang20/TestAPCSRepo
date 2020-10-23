d0e9c4b2938948832ff76db265f846e5e08be63c
mwang20 <mwang20@stuy.edu>
2020-10-23 14:48:33 -0400

    I think I got the arrToString and arrDeepToString methods to work

--- Demo.java
+++ Demo.java
@@ -2,6 +2,10 @@ public class Demo{
   public static void main(String[] args){
     int pyramid = Integer.parseInt(args[0]);
     printLoop(pyramid);
+    int[] n = {5, 4, 3, 2, 1};
+    int[][] test = {{5, 4, 3, 2, 1},{1, 2, 3, 4, 5}};
+    arrToString(n);
+    arrDeepToString(test);
   }
   public static void printLoop(int n){
     for(int i = 1; i <= n; i++ ){
@@ -11,4 +15,13 @@ public class Demo{
       System.out.println();
     }
   }
+   public static void arrToString(int []arr){
+      for ( int i = 0; i < arr.length; i++ )
+          System.out.print(arr[i]);
+   }
+   public static void arrDeepToString(int [][]arr){
+      System.out.println();
+      for ( int i = 0; i < arr.length; i++ )
+          System.out.print(arr[i][i]);
+   }
 }
